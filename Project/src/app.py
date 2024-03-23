7570699ecd5c1d1ee44248810bc30e899795c837
Leander Pedro <lpedr3@illinois.edu>
2024-03-23 16:38:25 -0500

    updated code

--- Project/src/app.py
+++ Project/src/app.py
@@ -16,50 +16,54 @@ def index():
 def generate_plan():
     exercise_type = request.form['exercise-type']
     current_weight = request.form['current-weight']
-    target_weight = request.form['target-weight']
-    current_maintenance = request.form['current-maintenance'] 
+    target_weight = request.form['target-weight'] 
 
     if target_weight < current_weight:
-        macroplan = """Calories: """ + (int(current_maintenance) - 200) +\
-            """Protein: """ + int(current_weight) + """g""" +\
-            """Fat: """ + (float(current_maintenance) - 200) * 0.25 +\
-            """Carbohydrates: """ + (float(current_maintenance)) * 0.5
+        macroplan_loss = """Calories: """ + str((int(current_weight) * 15) - 200) +\
+            """ Protein: """ + str(int(current_weight)) + """g""" +\
+            """ Fat: """ + str((((float(current_weight) * 15) - 200) * 0.25) / 9) + """g""" +\
+            """ Carbohydrates: """ + str((((float(current_weight) * 15) - 200) * 0.5) / 4) + """g"""
         if exercise_type == 'core':
             plan = """Goal = Weight Loss: 
             3 repeats (3 sets/12 reps crunches, 3 sets/12 reps bicycle kicks, 3 set/12 reps flutter kick)
-            1 min rest between sets 5 min between repeats"""
+            1 min rest between sets 5 min between repeats""" +\
+            """ Macros: """ + macroplan_loss
         elif exercise_type == 'legs':
             plan = """Goal = Weight Loss:
             3 repeats of (3 sets of 15 lightweight squats|3 sets of 30 walking lunges|3 sets of 10 bulgarian split squats|0.5 mile run)
-            2 minute rest between sets 5 minute rest between repeats"""
+            2 minute rest between sets 5 minute rest between repeats""" +\
+            """ Macros: """ + macroplan_loss
         else:  # upper-body
             plan = """Goal = Weight Loss:
             3 repeats of (3 sets of 5 pullups|3 sets of 12 light bench press|3 sets of 8 pushups|2 sets till failure light tricep extension)
-            1 minute rest inbetween sets 5 inbetween repeats"""
+            1 minute rest inbetween sets 5 inbetween repeats""" +\
+            """ Macros: """ + macroplan_loss
     elif target_weight > current_weight:
-        macroplan = """Calories: """ + (int(current_maintenance) + 200) +\
-            """Protein: """ + int(current_weight) + """g""" +\
-            """Fat: """ + (float(current_maintenance) + 200) * 0.25 +\
-            """Carbohydrates: """ + (float(current_maintenance)) * 0.5
+        macroplan_gain = """Calories: """ + str((int(current_weight) * 15) + 200) +\
+            """ Protein: """ + str(int(current_weight)) + """g""" +\
+            """ Fat: """ + str((((float(current_weight) * 15) + 200) * 0.25) / 9) + """g""" +\
+            """ Carbohydrates: """ + str((((float(current_weight) * 15) + 200) * 0.5) / 4) + """g"""
         if exercise_type == 'core':
             plan = """Goal = Weight Gain: 
             1x(2 sets/5 reps weighted crunches|2 sets/30 reps hanging leg raises|2 sets/12 reps medicine ball throw)
             3 minute rest between all sets"""
+            """ Macros: """ + macroplan_gain
         elif exercise_type == 'legs':
             plan = """Goal = Weight Gain:
             1x(2 sets of 5 heavy squat|2 sets of 12 heavy leg press|2 sets of bulgarian split squats till failure)
             3-5 minute rest inbetween sets"""
+            """ Macros: """ + macroplan_gain
         else:  # upper-body
             plan = """Goal = Weight Gain:
             1x(2 sets of 5 heavy bench|2 sets of 12 heavy incline bench press|2 sets of 5 pullups|1 set until failure heavy dips)
-            3 minute rest between sets"""
+            3 minute rest between sets""" +\
+            """ Macros: """ + macroplan_gain
 
     return jsonify({
         'exercise_type': exercise_type,
         'current_weight': current_weight,
         'target_weight': target_weight,
-        'plan': plan,
-        'macroplan' : macroplan
+        'plan': plan
     })
 
 if __name__ == '__main__':
